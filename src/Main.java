e3f048c7079b1a14ee7bcb4c94e6611fd55b6e77
Cristian A. Rossi <110430454+crisrossi1989@users.noreply.github.com>
2023-03-21 15:05:05 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String[] equipo;
         String archivoPronostico, archivoResultado;
-        int posPrimerComaPron, posPimerComaResul, apuesta,puntosTotales=0;
+        int posPrimerComaPron, posPimerComaResul, apuesta, puntosTotales=0;
         Equipo equipoL;
         Equipo equipoV;
         Pronostico prono=new Pronostico();
